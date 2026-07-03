7b248cc0392573cca091df5b117d49f1e60a1152
nicoPop1605 <nicoleta.bianca.pop@stud.ubbcluj.ro>
2026-07-03 20:09:56 +0300

    Delete calendar_backend directory

--- calendar_backend/src/main/java/com/calendar_backend/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.calendar_backend.repository;
-
-
-import com.calendar_backend.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
